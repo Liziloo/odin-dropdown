505193bc5cc571f5ab3a8e56554ffd7daaaf9d4f
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-01-13 12:58:09 -0500

    Add very basic styling to dropdown

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { dropdownMenu } from "./modules/dropdown";
+import './styles/comeau-reset.css';
+import './styles/styles.css';
 
 (function() {
     dropdownMenu();
--- src/modules/dropdown.js
+++ src/modules/dropdown.js
@@ -2,7 +2,9 @@ export { dropdownMenu };
 
 const dropdownMenu = () => {
     const dropdownHeader = document.querySelector('.dropdown-header');
-    const dropdownList = document.querySelector('.dropdown');
+    const dropdownList = document.querySelector('.dropdown-list');
+
+    dropdownList.hidden = true;
 
     dropdownHeader.addEventListener('click', () => {
         if (!dropdownList.hidden === true) {
